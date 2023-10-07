2bdc572ac32d45a0b8feecf62f32c502473c4422
CristianGariboldi <gariboldicristian@gmail.com>
2023-10-07 09:59:27 +0800

    feat: optimized neural network structures

--- scripts/neural_network_brake.py
+++ scripts/neural_network_brake.py
@@ -13,14 +13,14 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 
-# Load the CSV file into a DataFrame
-data = pd.read_csv('braking_robobus_final.csv')
-dataa = pd.read_csv('braking_robobus_final.csv')
 
-# Standardize data
-threshold0 = 2.2
-threshold1 = 2
-threshold2 = 1.7
+data = pd.read_csv('braking.csv')
+dataa = pd.read_csv('braking.csv')
+
+# Standardize data and remove outliers
+threshold0 = 2 #1.9
+threshold1 = 1.9  #1.7
+threshold2 = 1.8  #1.6
 
 mean0 = data["Velocity"].mean()
 std0 = data["Velocity"].std()
@@ -52,14 +52,12 @@ dataa = dataa[abs(dataa["Acceleration_with_pitch_comp"]-mean2) <= std2*threshold
 
 
 
-# Split the data into input features (velocity and acceleration) and target (command)
+# Split the data into input features (velocity and braking) and target (acceleration)
 
 X = data[['Velocity', 'Braking']].values
 y = data['Acceleration_with_pitch_comp'].values
 
 
-
-# Split the data into training and testing sets (e.g., 80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
@@ -70,30 +68,42 @@ y_train = torch.tensor(y_train, dtype=torch.float32)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_test = torch.tensor(y_test, dtype=torch.float32)
 
-# Define a custom PyTorch model
+
+# NN model
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(2, 128)  # Input layer with 2 neurons, hidden layer with n neurons
-        self.sigmoid1 = nn.Sigmoid()
-        self.fc2 = nn.Linear(128, 64)
-        self.sigmoid2 = nn.Sigmoid()
-        self.fc3 = nn.Linear(64, 1)  # Output layer with 1 neuron
+        self.fc1 = nn.Linear(2, 512)  # Input layer with 2 neurons, hidden layer with n neurons
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 1024)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(1024, 2048)  
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(2048, 256)
+        self.relu4 = nn.ReLU()
+        self.fc5 = nn.Linear(256, 64)
+        self.relu5 = nn.ReLU()
+        self.fc6 = nn.Linear(64, 1)     # Output layer with 1 neuron
         
-
     def forward(self, x):
         x = self.fc1(x)
-        x = self.sigmoid1(x)
+        x = self.relu1(x)
         x = self.fc2(x)
-        x = self.sigmoid2(x)
+        x = self.relu2(x)
         x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
+        x = self.relu4(x)
+        x = self.fc5(x)
+        x = self.relu5(x)
+        x = self.fc6(x)
         return x
 
-# Create an instance of the model
+
 model = NeuralNetwork()
 
-# Define a loss function and optimizer
+
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -103,21 +113,21 @@ for epoch in range(num_epochs):
     # Forward pass
     outputs = model(X_train)
     
-    loss = criterion(outputs, y_train.view(-1, 1))  # Calculate the loss
+    loss = criterion(outputs, y_train.view(-1, 1))  
 
     # Backpropagation and optimization
-    optimizer.zero_grad()  # Zero the gradients
-    loss.backward()  # Backpropagate the gradients
-    optimizer.step()  # Update the weights
+    optimizer.zero_grad()  
+    loss.backward()  
+    optimizer.step() 
 
 # Evaluate the model on the test data
 with torch.no_grad():
     test_outputs = model(X_test)
     test_loss = criterion(test_outputs, y_test.view(-1, 1))
-    print(f"Mean Squared Error on Test Data: {test_loss.item()}")
+    #print(f"Mean Squared Error on Test Data: {test_loss.item()}")
 
-# Make predictions on new data
-new_data = np.array([[(4-mean0)/std0, (7-mean1)/std1], [(0.5-mean0)/std0, (50-mean1)/std1]])  # Example new data with velocity and acceleration
+# Example: make predictions on new data
+new_data = np.array([[(4-mean0)/std0, (7-mean1)/std1], [(0.5-mean0)/std0, (50-mean1)/std1]])  
 #new_data = scaler.transform(new_data)  # Normalize the new data
 new_data = torch.tensor(new_data, dtype=torch.float32)
 with torch.no_grad():
@@ -127,18 +137,16 @@ with torch.no_grad():
         print(f"Data {i + 1}: {pred.item()}")
 
 # Visualization
-# Create a meshgrid for velocity and acceleration
+
 #velocity_range = np.linspace((X[:, 0]*std0+mean0).min(), (X[:, 0]*std0+mean0).max(), 100)
 #braking_range = np.linspace((X[:, 1]*std1+mean1).min(), (X[:, 1]*std1+mean1).max(), 100)
 velocity_range = np.linspace(0, (X[:, 0]*std0+mean0).max(), 100)
 braking_range = np.linspace((X[:, 1]*std1+mean1).min(), (X[:, 1]*std1+mean1).max(), 100)
 V, A = np.meshgrid(velocity_range, braking_range)
 
-# Create a grid of input data points for prediction
 input_grid = np.column_stack(((V.flatten()-mean0)/std0, (A.flatten()-mean1)/std1))
 input_grid = torch.tensor(input_grid, dtype=torch.float32)
 
-# Predict the throttling commands for the entire input grid
 with torch.no_grad():
     commands = model(input_grid).reshape(V.shape)
     
@@ -147,54 +155,42 @@ commands_new = commands*std2+mean2
     
 
 # Save the trained model
-#torch.save(model.state_dict(), 'trained_brake_model.pth')
+torch.save(model.state_dict(), 'trained_brake.pth')
 
 
-# Calculate MSE
+# evaluation
 mse = mean_squared_error(y_test, test_outputs.view(-1).numpy())
 print(f"Mean Squared Error on Test Data: {mse}")
 
-
-# Calculate MAE
 mae = mean_absolute_error(y_test, test_outputs.view(-1).numpy())
 print(f"Mean Absolute Error on Test Data: {mae}")
-
 
 rmse = np.sqrt(mse)
 print(f"Root Mean Squared Error on Test Data: {rmse}")
 
-
-# Calculate R2 score
 r2 = r2_score(y_test, test_outputs.view(-1).numpy())
 print(f"R-squared (R2) Score on Test Data: {r2}")
 
 
-
+# visualize raw data with the NN model for comparison
 xdata = dataa.Velocity*std0+mean0
 ydata = dataa.Braking*std1+mean1
 zdata = dataa.Acceleration_with_pitch_comp*std2+mean2
 
-# Create the 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-
-# Plot the surface
 scatter = ax.scatter3D(xdata, ydata, zdata, c=zdata, marker='o')
 surf = ax.plot_surface(V, A, commands_new, cmap='viridis')
 
 
-
-# Customize the plot
 ax.set_xlabel('Velocity')
 ax.set_zlabel('Acceleration')
 ax.set_ylabel('Braking Output')
 ax.set_title('Neural Network Output vs. Velocity and Braking')
 
-# Add a color bar which maps values to colors
 fig.colorbar(surf)
 
-# Show the plot
 plt.show()
 
 
--- scripts/neural_network_throttle.py
+++ scripts/neural_network_throttle.py
@@ -12,16 +12,16 @@ from scipy.signal import medfilt
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
+import math
 
 
-# Load the CSV file into a DataFrame
-data = pd.read_csv('throttling_robobus_final.csv')
-dataa = pd.read_csv('throttling_robobus_final.csv')
+data = pd.read_csv('throttling.csv')
+dataa = pd.read_csv('throttling.csv')
 
-# Standardize data
-threshold0 = 1.9
-threshold1 = 2
-threshold2 = 1.8
+# Standardize data and remove outliers according to thresholds
+threshold0 = 10 #6
+threshold1 = 10  #3
+threshold2 = 5 #4
 
 mean0 = data["Velocity"].mean()
 std0 = data["Velocity"].std()
@@ -48,14 +48,12 @@ data = data[abs(data["Acceleration_with_pitch_comp"]-mean2) <= std2*threshold2]
 dataa = dataa[abs(dataa["Acceleration_with_pitch_comp"]-mean2) <= std2*threshold2]
 
 
-# Split the data into input features (velocity and acceleration) and target (command)
+# Split the data into input features (velocity and throttle) and target (acceleration) and test/train
 
 X = data[['Velocity', 'Throttling']].values
 y = data['Acceleration_with_pitch_comp'].values
 
 
-
-# Split the data into training and testing sets (e.g., 80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
@@ -66,32 +64,37 @@ y_train = torch.tensor(y_train, dtype=torch.float32)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_test = torch.tensor(y_test, dtype=torch.float32)
 
-# Define a custom PyTorch model
+
+# NN model
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(2, 128)  # Input layer with 2 neurons, hidden layer with n neurons
-        self.sigmoid1 = nn.Sigmoid()
-        self.fc2 = nn.Linear(128, 64)
-        self.sigmoid2 = nn.Sigmoid()
-        self.fc3 = nn.Linear(64, 1)  # Output layer with 1 neuron
+        self.relu1 = nn.LogSigmoid()
+        self.fc2 = nn.Linear(128, 32)
+        self.relu2 = nn.LogSigmoid()
+        self.fc3 = nn.Linear(32, 1)  # Output layer with 1 neuron
         
-
+        
+        
     def forward(self, x):
         x = self.fc1(x)
-        x = self.sigmoid1(x)
+        x = self.relu1(x)
         x = self.fc2(x)
-        x = self.sigmoid2(x)
+        x = self.relu2(x)
         x = self.fc3(x)
+        
+        
         return x
 
-# Create an instance of the model
+
+
 model = NeuralNetwork()
 
-# Define a loss function and optimizer
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.001) #, weight_decay=0.001)
+
 
 # Training loop
 num_epochs = 100
@@ -99,21 +102,22 @@ for epoch in range(num_epochs):
     # Forward pass
     outputs = model(X_train)
     
-    loss = criterion(outputs, y_train.view(-1, 1))  # Calculate the loss
+    loss = criterion(outputs, y_train.view(-1, 1))  
 
     # Backpropagation and optimization
-    optimizer.zero_grad()  # Zero the gradients
-    loss.backward()  # Backpropagate the gradients
-    optimizer.step()  # Update the weights
+    optimizer.zero_grad()  
+    loss.backward()  
+    optimizer.step() 
 
-# Evaluate the model on the test data
+
 with torch.no_grad():
     test_outputs = model(X_test)
     test_loss = criterion(test_outputs, y_test.view(-1, 1))
-    print(f"Mean Squared Error on Test Data: {test_loss.item()}")
+    #print(f"Mean Squared Error on Test Data: {test_loss.item()}")
 
-# Make predictions on new data
-new_data = np.array([[(4-mean0)/std0, (7-mean1)/std1], [(0.5-mean0)/std0, (50-mean1)/std1]])  # Example new data with velocity and acceleration
+
+# Example: make predictions on new data
+new_data = np.array([[(4-mean0)/std0, (7-mean1)/std1], [(0.5-mean0)/std0, (50-mean1)/std1]]) 
 #new_data = scaler.transform(new_data)  # Normalize the new data
 new_data = torch.tensor(new_data, dtype=torch.float32)
 with torch.no_grad():
@@ -122,17 +126,17 @@ with torch.no_grad():
     for i, pred in enumerate(predictions):
         print(f"Data {i + 1}: {pred.item()}")
 
+
 # Visualization
-# Create a meshgrid for velocity and acceleration
+
 velocity_range = np.linspace((X[:, 0]*std0+mean0).min(), (X[:, 0]*std0+mean0).max(), 100)
-throttling_range = np.linspace((X[:, 1]*std1+mean1).min(), (X[:, 1]*std1+mean1).max(), 100)
+#throttling_range = np.linspace((X[:, 1]*std1+mean1).min(), (X[:, 1]*std1+mean1).max(), 100)
+throttling_range = np.linspace(0, (X[:, 1]*std1+mean1).max(), 100)
 V, A = np.meshgrid(velocity_range, throttling_range)
 
-# Create a grid of input data points for prediction
 input_grid = np.column_stack(((V.flatten()-mean0)/std0, (A.flatten()-mean1)/std1))
 input_grid = torch.tensor(input_grid, dtype=torch.float32)
 
-# Predict the throttling commands for the entire input grid
 with torch.no_grad():
     commands = model(input_grid).reshape(V.shape)
     
@@ -142,24 +146,19 @@ commands_new = commands*std2+mean2
 
 
 # Save the trained model
-#torch.save(model.state_dict(), 'trained_throttle_model.pth')
+torch.save(model.state_dict(), 'trained_throttle.pth')
 
 
-# Calculate MSE
+# evaluation
 mse = mean_squared_error(y_test, test_outputs.view(-1).numpy())
 print(f"Mean Squared Error on Test Data: {mse}")
 
-
-# Calculate MAE on the test data
 mae = mean_absolute_error(y_test, test_outputs.view(-1).numpy())
 print(f"Mean Absolute Error on Test Data: {mae}")
 
-
-rmse = np.sqrt(mse)
+rmse = math.sqrt(mse)
 print(f"Root Mean Squared Error on Test Data: {rmse}")
 
-
-# Calculate R2 score on the test data
 r2 = r2_score(y_test, test_outputs.view(-1).numpy())
 print(f"R-squared (R2) Score on Test Data: {r2}")
     
@@ -169,22 +168,18 @@ ydata = dataa.Throttling*std1+mean1
 zdata = dataa.Acceleration_with_pitch_comp*std2+mean2
 
 
-# Create the 3D plot
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Plot the surface
 scatter = ax.scatter3D(xdata, ydata, zdata, c=zdata, marker='o')
 surf = ax.plot_surface(V, A, commands_new, cmap='viridis')
 
-# Customize the plot
 ax.set_xlabel('Velocity')
 ax.set_zlabel('Acceleration')
 ax.set_ylabel('Throttling Output')
 ax.set_title('Neural Network Output vs. Velocity and Throttling')
 
-# Add a color bar which maps values to colors
 fig.colorbar(surf)
 
-# Show the plot
 plt.show()
