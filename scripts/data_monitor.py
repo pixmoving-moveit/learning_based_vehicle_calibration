5c46927c80ac6a2c5f9003c3799fe41ac6d6d27a
liuxueqing <xueqing.liu@pixmoving.net>
2026-03-04 14:06:33 +0800

    fix(topic):data_monitor 中, 修改imu和pitch的topic

--- scripts/data_monitor.py
+++ scripts/data_monitor.py
@@ -20,11 +20,15 @@ class DataMonitor(rclpy.node.Node):
         self.can_timestamp = 0.0
         super().__init__('data_monitor')
 
-        self.declare_parameter('pitch_topic', "/sensing/gnss/chc/pitch")
+        # self.declare_parameter('pitch_topic', "/sensing/gnss/chc/pitch")
+        self.declare_parameter('pitch_topic', "/sensing/gnss/pitch")
         self.declare_parameter('actuation_status_topic', "/vehicle/status/actuation_status")
         self.declare_parameter('steering_status_topic', "/vehicle/status/steering_status")
         self.declare_parameter('velocity_status_topic', "/vehicle/status/velocity_status")
-        self.declare_parameter('imu_topic', "/sensing/gnss/chc/imu")
+        # self.declare_parameter('imu_topic', "/sensing/gnss/chc/imu")
+        self.declare_parameter('imu_topic', "/sensing/gnss/imu")
+        
+        
 
         # Get topic names from parameters
         self.pitch_topic = self.get_parameter('pitch_topic').get_parameter_value().string_value
